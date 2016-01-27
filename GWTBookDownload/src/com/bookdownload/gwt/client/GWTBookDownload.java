66303d09183a74cc5d8ac350c3f74f325cb2e647
Nimishnalan <nimishnalan93@gmail.com>
2016-01-28 04:33:30 +0530

    Add author +Add Books +Update books
    UI module - design complete

--- GWTBookDownload/src/com/bookdownload/gwt/client/GWTBookDownload.java
+++ GWTBookDownload/src/com/bookdownload/gwt/client/GWTBookDownload.java
@@ -15,9 +15,11 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -43,34 +45,121 @@ public class GWTBookDownload implements EntryPoint {
 	 */
 
 	public void onModuleLoad() {
-		final Anchor tab_author = new Anchor();
 
-		RootPanel.get("tab_author").add(tab_author);
-		final TextBox txtbox = new TextBox();
-		tab_author.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
-				txtbox.setText("Author clicked");
-			}
-		});
-		
-		
-		
-		RootPanel.get("txttest").add(txtbox);
-		final Button btn = new Button();
-		btn.addClickHandler(new ClickHandler() {
+		// AUTHOR TAB FUNCTIONS
+		final Button btn_submit = new Button("Submit");
+		final TextBox txt_author_name = new TextBox();
+
+		// HANDLING AUTHOR TAB
+		btn_submit.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
 				// TODO Auto-generated method stub
-				txtbox.setText("Button clicked");
+				txt_author_name.setText("Submit was clicked");
 			}
 		});
 
-		
-		RootPanel.get("btn").add(btn);
+		RootPanel.get("txt_author_name").add(txt_author_name);
+		RootPanel.get("btn_submit").add(btn_submit);
+
+		// BOOKS TAB ADDING
+
+		// adding llist_author
+		final ListBox list_author = new ListBox();
+
+		// for loop for each item to be added
+		list_author.addItem("Author1");
+		list_author.addItem("Author1");
+		list_author.addItem("Author1");
+		list_author.addItem("Author1");
+		list_author.addItem("Author1");
+		list_author.setVisibleItemCount(1);
+		RootPanel.get("list_author").add(list_author);
+
+		// adding txt_isbn
+		TextBox txt_isbn = new TextBox();
+		RootPanel.get("txt_isbn").add(txt_isbn);
+
+		// adding txt_book_name
+		TextBox txt_book_name = new TextBox();
+		RootPanel.get("txt_book_name").add(txt_book_name);
+
+		// adding txt_publisher_name
+		TextBox txt_publisher_name = new TextBox();
+		RootPanel.get("txt_publisher_name").add(txt_publisher_name);
+
+		// adding date_picker
+		DatePicker date_picker = new DatePicker();
+		RootPanel.get("date_picker").add(date_picker);
+
+		// adding list_book_category
+		final ListBox list_book_category = new ListBox();
+
+		// for loop for each item to be added
+		list_book_category.addItem("Sci Fi");
+		list_book_category.addItem("Spiritual");
+		list_book_category.setVisibleItemCount(3);
+		RootPanel.get("list_book_category").add(list_book_category);
+
+		// adding btn_book_reset
+		Button btn_book_reset = new Button("Reset");
+		RootPanel.get("btn_book_reset").add(btn_book_reset);
+
+		// adding btn_book_reset
+		Button btn_book_submit = new Button("Submit");
+		RootPanel.get("btn_book_submit").add(btn_book_submit);
+
+		// ============================================
+		// ============================================
+		// UPDATE
+		// ============================================
+		// ============================================
+
+		// adding llist_author
+		final ListBox update_list_author = new ListBox();
+
+		// for loop for each item to be added
+		update_list_author.addItem("Author1");
+		update_list_author.addItem("Author1");
+		update_list_author.addItem("Author1");
+		update_list_author.addItem("Author1");
+		update_list_author.addItem("Author1");
+		update_list_author.setVisibleItemCount(1);
+		RootPanel.get("update_list_author").add(update_list_author);
+
+		// adding txt_isbn
+		TextBox update_txt_isbn = new TextBox();
+		RootPanel.get("update_txt_isbn").add(update_txt_isbn);
+
+		// adding txt_book_name
+		TextBox update_txt_book_name = new TextBox();
+		RootPanel.get("update_txt_book_name").add(update_txt_book_name);
+
+		// adding txt_publisher_name
+		TextBox update_txt_publisher_name = new TextBox();
+		RootPanel.get("update_txt_publisher_name").add(update_txt_publisher_name);
+
+		// adding date_picker
+		DatePicker update_date_picker = new DatePicker();
+		RootPanel.get("update_date_picker").add(update_date_picker);
+
+		// adding list_book_category
+		final ListBox update_list_book_category = new ListBox();
+
+		// for loop for each item to be added
+		update_list_book_category.addItem("Sci Fi");
+		update_list_book_category.addItem("Spiritual");
+		update_list_book_category.setVisibleItemCount(3);
+		RootPanel.get("update_list_book_category").add(update_list_book_category);
+
+		// adding btn_book_reset
+		Button update_btn_book_reset = new Button("Reset");
+		RootPanel.get("update_btn_book_reset").add(update_btn_book_reset);
+
+		// adding btn_book_reset
+		Button update_btn_book_submit = new Button();
+		RootPanel.get("update_btn_book_submit").add(update_btn_book_submit);
 
 	}
 }
